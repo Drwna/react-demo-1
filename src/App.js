d2fe758a5d29afe1abf8c7a131ab88c6541045e6
draganet <3417237276@qq.com>
2021-11-21 00:23:43 +0800

    component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,36 @@ import React from "react";
 const App = () => {
   return (
     // React.createElement()
-    <div>App 组件</div>
+    <div>
+      App 组件
+      <Component />
+      <Loop numbers={[1, 2, 3, 4]} />
+    </div>
   );
 };
+
+const Component = () => {
+  let inner;
+  let n = window.prompt("enter a number");
+  if (n % 2 === 0) {
+    inner = <div>n是偶数</div>;
+  } else {
+    inner = <span>n是奇数</span>;
+  }
+  const content = <div>{inner}</div>;
+  return content;
+};
+
+const Loop = (props) => {
+  const array = [];
+  for (let i = 0; i < props.numbers.length; i++) {
+    array.push(
+      <div key={i}>
+        下标为{i}值为{props.numbers[i]}
+      </div>
+    );
+  }
+  return <div>{array}</div>;
+};
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
-const Component = () => {
-  let inner;
-  let n = 4;
-  if (n % 2 === 0) {
-    inner = <div>n是偶数</div>;
-  } else {
-    inner = <span>n是奇数</span>;
-  }
-  const content = <div>{inner}</div>;
-  return content;
-};
 
-ReactDOM.render(<Component />, document.querySelector("#root2"));
+
+
+
