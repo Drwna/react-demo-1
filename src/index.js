e7d98ad0c8f125234a910d81c4524f7ed692c9ab
draganet <3417237276@qq.com>
2021-11-20 23:49:16 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Component = () => {
+  let inner;
+  let n = 4;
+  if (n % 2 === 0) {
+    inner = <div>n是偶数</div>;
+  } else {
+    inner = <span>n是奇数</span>;
+  }
+  const content = <div>{inner}</div>;
+  return content;
+};
+
+ReactDOM.render(<Component />, document.querySelector("#root2"));
